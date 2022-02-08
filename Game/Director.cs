1e17bfefa6a76e896a83d8b6580f2ae8fbc8ada6
Ebenezer Mensah <mebenezer2446@gmail.com>
2022-02-08 14:57:29 -0700

    ok

--- Game/Director.cs
+++ Game/Director.cs
@@ -1,19 +1,69 @@
 using System;
-using System.Collections.Generic;
-
 namespace jumper.Game
 {
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
     public class Director
-    {  
-        
-        public void StartGame()
+    {
+        private Jumper jumper = new Jumper();
+        private bool isPlaying = true;
+        private Word word = new Word();
+        private TerminalService terminalService = new TerminalService();
+
+        /// <summary>
+        /// Constructs a new instance of Director.
+        /// </summary>
+        public Director()
         {
-            //Ebenezer
-            //GetInputs
-            //DoUpdates
-            //DoOutputs
-            
         }
 
+        /// <summary>
+        /// Starts the game by running the main game loop.
+        /// </summary>
+        public void StartGame()
+        {
+            while (isPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            }
+        }
+
+        /// <summary>
+        /// Moves the seeker to a new location.
+        /// </summary>
+        private void GetInputs()
+        {
+            terminalService.WriteText(jumper.location.ToString());
+            int location = terminalService.ReadNumber("\nGuess a letter [a-z]: ");
+            word.MoveLocation(location);
+        }
+
+        /// <summary>
+        /// Keeps watch on where the seeker is moving.
+        /// </summary>
+        private void DoUpdates()
+        {
+            jumper.WatchWord(word);
+        }
+
+        /// <summary>
+        /// Provides a hint for the seeker to use.
+        /// </summary>
+        private void DoOutputs()
+        {
+            string hint = jumper.GetHint();
+            terminalService.WriteText(hint);
+            if (jumper.IsFound())
+            {
+                isPlaying = false;
+            }
+            
+        }
     }
 }
