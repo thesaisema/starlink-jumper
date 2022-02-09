b6aed31f8bef52370bf25a34aefd69134ec3e944
lemmonas <lem20016@byui.edu>
2022-02-09 11:52:36 -0700

    Created the methods to get the word and the guess

--- Game/Word.cs
+++ Game/Word.cs
@@ -8,7 +8,20 @@ namespace jumper.Game
         //Nwokolo - DisplayBlanks - displays the blank lines
         //Andra - UserGuess - returns the letter guessed by the user
         //Nwokolo - UpdateWord - updates the word after user guess. (could be done in DisplayWord)
+        public string GenerateWord(){
+            string[] wordList = {"flying", "jump", "death", "adventure", "crazy", "penguins", "manley"};
+            Random rnd = new Random();
+            int random = rnd.Next(0, (wordList.Length + 1));//code for generating a random word from the list
+            string randWord = wordList[random];
+            return randWord;
+        }
 
+        public string UserGuess(){
+            string guess;
+            Console.Write("Guess a letter [a-z]: ");
+            guess = Console.ReadLine();
+            return guess;
+        }
 
     }
 }
