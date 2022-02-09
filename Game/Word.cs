d54371a8ff516f14e601122ec166fd93a548786e
nwokolo <nwokolobueze@gmail.com>
2022-02-09 12:16:29 -0700

    Connecting the different classes

--- Game/Word.cs
+++ Game/Word.cs
@@ -7,14 +7,14 @@ namespace jumper.Game
     public class Word
     {  
         // Andra can come up with the method that creates random words here
-        static string RandomWord = "flying";
+        static string RandomWord = GenerateWord();
 
         // Get the lenth of the random word
         static int LenOfRandomWord = RandomWord.Length;
         Jumper jump = new Jumper();
 
         // This method creates and displays dashes
-        public List<string> DisplayDash()
+        public List<string> CreateDash()
         {
             List<string> Dashes = new List<string>();
             for (int i=0; i<LenOfRandomWord; i++)
@@ -25,14 +25,14 @@ namespace jumper.Game
         }
 
         // This method updates and displays dashes or cuts parachute
-        public void UpdateAndDisplayDash()
+        public void DisplayDash()
         {
             // Daniel will create the parachute
             int LenOfParachute = jump.LenOfParachut - 3;
             // This line keeps count of the number of letters that a user has guessed correctly
             int letter_count = LenOfRandomWord;
 
-            List<string> CurrentDash = DisplayDash();
+            List<string> CurrentDash = CreateDash();
             while (LenOfParachute > 0 && letter_count > 0)
             {
                 // This line displays the number of letters the user has gotten correctly
@@ -42,9 +42,7 @@ namespace jumper.Game
 
                 jump.DisplayParachute();
 
-                // Ask User for a letter
-                System.Console.Write("\n\nGuess a letter [a-z]: ");
-                string UserGuess = Console.ReadLine();
+                
 
                 // This checks if the letter is in the randomword or not.
                 // If the letter is in the randomword, currenDash gets updated 
